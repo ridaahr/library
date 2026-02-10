af08a4a23e8d32cacd7c8343c6d9afbfb55cdc3f
ridaahr <raharrar12@gmail.com>
2026-02-10 13:09:20 +0100

    Delete Models directory

--- Models/User.js
+++
@@ -1,9 +0,0 @@
-class User {
-    constructor(id, name, user, password, role) {
-        this.id = id;
-        this.name = name;
-        this.user = user;
-        this.password = password;
-        this.role = role;
-    }
-}
